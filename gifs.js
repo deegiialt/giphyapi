282b41e1a2f418958201e08aaf098cb3d85b18d5
DeegiiAlt <aagiideegii@gmail.com>
2018-01-31 19:49:26 -0700

    added folders, some JS

--- gifs.js
+++ gifs.js
@@ -1,4 +1,6 @@
 // On page load:
+$(document).ready( function() {
+
 	//set variables:
 		//buttonTitles = ["", "", "", ""]
 	//function displayButtons
@@ -42,6 +44,49 @@
 					//else if (imageState == " animated")
 						//set src attribute of image clicked to be the data-stillurl attribute of the image clicked
 						//set data-state attribute of image clicked to be "still"
+});
 					
 
-
+ // On page load:
+            // Set variables:
+                // buttonTitles = ["baseball", "hockey", "football"]
+            // Run displayButtons function
+        // displayButtons - function
+            // Clear previous buttons from screen
+            // for loop through buttonTitles
+                // Create a jQuery button
+                // Add attribute to jQuery button created (attribute name: "button-title", attribute value: the button title at that index)
+                // Put the current buttonTitle that we are looping through in the button
+                // Append button to page
+        // When the user clicks one of the buttons - function
+            // prevent default
+            // get the attribute of the button clicked, and store in a variables
+            // clear out old images from the page (.empty)
+            // AJAX call to GIPHY
+                // Method | GET
+                // URL | https://api.giphy.com/v1/gifs/search?api_key=YOUR_API_KEY&q=(THE BUTTON ATTRIBUTE WE GOT EARLIER)
+                // .done
+                    // loop through response.data
+                        // create a jQuery div
+                        // create a jQuery image
+                        // Set the src attribute of the jQuery image to be image that we are looping through
+                        // Add data-state attribute to jQuery image = "still"
+						// Add data-animateurl attribute to jQuery image
+						// Add data-stillurl attribute to jQuery image
+                    	// create a jQuery paragrapgh
+                        // Put the rating from GIPHY response into the paragrapgh created
+                        // Append jQuery image to jQuery div
+                        // Append jQuery paragrapgh to jQuery div
+                        // Append jQuery div to page
+        // On click of form submit button - function
+            // Create variable of user input text field
+            // Push variable just created to array (buttonTitles)
+            // Run displayButtons function
+        // On click of image div - function
+            // Set variable equal to image clicked data-state attribute
+            // if (imageState == "still")
+                // Set src attribute of image clicked to be data-animateurl attribute of the image clicked
+                // Set data-state attribute of image clicked to be "animated"
+            // else if (imageState == "animated")
+				// Set src attribute of image clicked to be data-stillurl attribute of the image clicked
+				// Set data-state attribute of image clicked to be "still"
